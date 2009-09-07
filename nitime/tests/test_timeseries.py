41c032395ce3dc00abf87b7cfcba9898d063d6b8
arokem <arokem@berkeley.edu>
2009-09-06 23:17:40 -0700

    Small changes in test_timeseries

--- nitime/tests/test_timeseries.py
+++ nitime/tests/test_timeseries.py
@@ -19,9 +19,11 @@ def test_CorrelationAnalyzer():
     #Test the self-sameness: correlation(x,x)==1
     np.testing.assert_equal(C.correlation[0,0],1)
     np.testing.assert_equal(C.correlation[1,1],1)
+
     #Test the cross-correlation:
     #First the symmetry:
     np.testing.assert_array_almost_equal(C.xcorr.data[0,1],C.xcorr.data[1,0])
+    
     #Test the normalized cross-correlation
     #Should be equal to 1 at 0 time-lag:
     np.testing.assert_equal(C.xcorr_norm.data[0,0,C.xcorr_norm.time==0],1)
