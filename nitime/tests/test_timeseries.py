3e5aefbc18fb1d5dcfe50598fae652b07bbfc762
arokem <arokem@berkeley.edu>
2009-09-21 15:40:13 -0700

    Test for event-related

--- nitime/tests/test_timeseries.py
+++ nitime/tests/test_timeseries.py
@@ -32,10 +32,32 @@ def test_CorrelationAnalyzer():
     #And the auto-correlation should be equal to 1 at 0 time-lag:
     np.testing.assert_equal(C.xcorr_norm.data[0,0,C.xcorr_norm.time==0],1)
 
+    #Does it depend on having an even number of time-points?
+    #make another time-series with an odd number of items:
+    t = np.arange(1023)
+    x = np.sin(10*t) + np.random.rand(t.shape[-1])
+    y = np.sin(10*t) + np.random.rand(t.shape[-1])
+
+    T = ts.UniformTimeSeries(np.vstack([x,y]),sampling_rate=Fs)
+
+    C = ts.CorrelationAnalyzer(T)
 
     
+    np.testing.assert_equal(C.xcorr_norm.data[0,1,C.xcorr_norm.time==0]
+                            ,C.correlation[0,1])
 
-    
-    
 
-    
+def test_EventRelatedAnalyzer():
+
+    cycles = 10
+    l = 1024
+    t = np.linspace(0.,2*np.pi,l)
+    signal = np.sin(cycles*t)
+    events = np.zeros(t.shape)
+    #An event occurs at the beginning of every cycle:
+    events[np.arange(0,l-(l/cycles),l/cycles)]=1
+    events[np.arange(l/cycles/2,l-(l/cycles),l/cycles)]=2
+    T_signal = ts.UniformTimeSeries(signal,sampling_rate=1)
+    T_events = ts.UniformTimeSeries(events,sampling_rate=1)
+    E = ts.EventRelatedAnalyzer(T_signal,T_events,l/(cycles*2))
+
