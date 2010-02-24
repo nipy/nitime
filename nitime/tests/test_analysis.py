6ca14d52584dca21a0fad39ad6e66bd1210a81e0
arokem <arokem@berkeley.edu>
2010-02-24 15:23:41 -0800

    Added test for the SparseCoherenceAnalyzer, fixes in the CoherenceAnalyzer

--- nitime/tests/test_analysis.py
+++ nitime/tests/test_analysis.py
@@ -34,9 +34,8 @@ def test_SpectralAnalyzer():
     npt.assert_equal(c.shape,(2,2,t.shape[0]/2+1))
 
 def test_CoherenceAnalyzer():
-
     Fs = np.pi
-    t = np.arange(1024)
+    t = np.arange(10)
     x = np.sin(10*t) + np.random.rand(t.shape[-1])
     y = np.sin(10*t) + np.random.rand(t.shape[-1])
     T = ts.UniformTimeSeries(np.vstack([x,y]),sampling_rate=Fs)
@@ -47,12 +46,23 @@ def test_CoherenceAnalyzer():
     npt.assert_equal(C.coherence[0,1],C.coherence[1,0])
     #Phase/delay asymmetry:
     npt.assert_equal(C.phase[0,1],-1*C.phase[1,0])
-    npt.assert_equal(C.delay[0,1],-1*C.delay[1,0])
+    npt.assert_equal(C.delay[0,1][1:],-1*C.delay[1,0][1:]) #The very first one
+                                        #is a nan
 
     #Calculation of the spectrum is the same as in the default spectral analyzer:
     S = nta.SpectralAnalyzer(T)
     npt.assert_equal(S(),(C.frequencies,C.spectrum))
-    
+
+def test_SparseCoherenceAnalyzer():
+    Fs = np.pi
+    t = np.arange(10)
+    x = np.sin(10*t) + np.random.rand(t.shape[-1])
+    y = np.sin(10*t) + np.random.rand(t.shape[-1])
+    T = ts.UniformTimeSeries(np.vstack([x,y]),sampling_rate=Fs)
+    C = nta.SparseCoherenceAnalyzer(T,ij=((0,1),(1,0)))
+
+    #Coherence symmetry:
+    npt.assert_equal(np.abs(C[0,1]),np.abs(C[1,0]))
 
 def test_CorrelationAnalyzer():
 
@@ -66,10 +76,10 @@ def test_CorrelationAnalyzer():
     C = nta.CorrelationAnalyzer(T)
 
     #Test the symmetry: correlation(x,y)==correlation(y,x) 
-    npt.assert_equal(C.correlation[0,1],C.correlation[1,0])
+    npt.assert_equal(C[0,1],C[1,0])
     #Test the self-sameness: correlation(x,x)==1
-    npt.assert_equal(C.correlation[0,0],1)
-    npt.assert_equal(C.correlation[1,1],1)
+    npt.assert_equal(C[0,0],1)
+    npt.assert_equal(C[1,1],1)
 
     #Test the cross-correlation:
     #First the symmetry:
@@ -78,7 +88,7 @@ def test_CorrelationAnalyzer():
     #Test the normalized cross-correlation
     #The cross-correlation should be equal to the correlation at time-lag 0
     npt.assert_equal(C.xcorr_norm.data[0,1,C.xcorr_norm.time==0]
-                            ,C.correlation[0,1])
+                            ,C[0,1])
 
     #And the auto-correlation should be equal to 1 at 0 time-lag:
     npt.assert_equal(C.xcorr_norm.data[0,0,C.xcorr_norm.time==0],1)
@@ -92,10 +102,9 @@ def test_CorrelationAnalyzer():
     T = ts.UniformTimeSeries(np.vstack([x,y]),sampling_rate=Fs)
 
     C = nta.CorrelationAnalyzer(T)
-
     
     npt.assert_equal(C.xcorr_norm.data[0,1,C.xcorr_norm.time==0]
-                            ,C.correlation[0,1])
+                            ,C[0,1])
 
 def test_EventRelatedAnalyzer():
 
