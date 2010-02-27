eb34b426b0270008836a0b41c4532af88cb33dae
arokem <arokem@berkeley.edu>
2010-02-27 10:42:44 -0800

    Changes in vista_utils, in indexing order using ROI coords. Need to verify whether this really works in all cases

--- nitime/vista_utils.py
+++ nitime/vista_utils.py
@@ -37,7 +37,7 @@ def getROIcoords(ROI_file):
     
 
 ##---- getTseries: -----------------------------------------------
-def get_time_series_inplane(coords,time_series_file,
+def get_time_series_inplane(coords,scan_file,
                                   f_c=0.01,up_sample_factor=[1,1,1],
                                   detrend=True,normalize=True,average=True,
                                   TR=None):
@@ -50,7 +50,7 @@ def get_time_series_inplane(coords,time_series_file,
         each array holds the X,Y,Z locations of an ROI
         (as represented in the Inplane)
 
-    time_series_file: string, full path to the analyze file of the scan
+    scan_file: string, full path to the analyze file of the scan
 
     TR: float the repetition time in the experiment
     
@@ -82,39 +82,34 @@ def get_time_series_inplane(coords,time_series_file,
     => average (across voxels, on a time-point-by-time-point basis)
 
     """
-
-    #If we got a list of coord arrays, we're happy. Otherwise, we want to force
-    #our input to be a list:
-    try:
-        coords.shape #If it is an array, it has a shape, otherwise, we 
-        #assume it's a list. If it's an array, we want to
-        #make it into a list:
-        coords = [coords]
-    except: #If it's a list already, we don't need to do anything:
-        pass
-    
-    #Make a list the size of the coords-list, with place holder 0's: 
-    newCoords = list([0]) * len(coords) 
+    from nipy.io.imageformats import load
     
     #Get the nifti image object
-    print('Reading file: ' + time_series_file)
+
+    print 'Reading data from %s' %scan_file 
+    data = load(scan_file).get_data() #if using nipy.io.imageformats.load
 
     #Adjusted the coordinates according to the ratio between the
     #sampling in the gray and the sampling in the inplane, move the
     #slice dimension to be the first one and change the indexing from
-    #1-based to 0-based:
+    #1-based to 0-based, also adjust the coords order, according to the mrVista
+    #weird conventions: 
+
+    this_data = data[np.round(coords[1]/up_sample_factor[1]).astype(int)-1,
+                         np.round(coords[0]/up_sample_factor[0]).astype(int)-1,
+                         np.round(coords[2]/up_sample_factor[2]).astype(int)-1]
+
+    if normalize:
+        this_data = tsu.percent_change(this_data)
+
+    if average:
+        this_data = np.mean(this_data,0)
         
-    for i in xrange(len(coords)):            
-        newCoords[i] = np.empty(coords[i].shape,dtype='int')
-        newCoords[i][0,:] = coords[i][0,:] / up_sample_factor[0] - 1 #Slices 
-        newCoords[i][1,:] = coords[i][1,:] / up_sample_factor[1] - 1 #Inplane
-        newCoords[i][2,:] = coords[i][2,:] / up_sample_factor[2] - 1 #Inplane
-        time_series =  ts.time_series_from_file(time_series_file,
-                                             newCoords,
-                                             normalize=normalize,
-                                             detrend=detrend,
-                                             average=average,
-                                             f_c=f_c,TR=TR)
+    time_series = ts.UniformTimeSeries(data=this_data,sampling_interval=TR)
+
+    if detrend:
+        F = ta.FilterAnalyzer(this_bold,lb=f_c)
+        time_series = F.filtered_boxcar
         
     return time_series
 
