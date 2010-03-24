c0ff999a79a7e6e865e6241ae99c10a04cc63dc3
arokem <arokem@berkeley.edu>
2010-03-24 14:04:04 -0700

    Added back fmri examples

---
+++ doc/examples/fmri1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+#Import from other libraries:
+import numpy as np
+from matplotlib.pyplot import figure
+from matplotlib.mlab import csv2rec
+
+#Import the time-series objects: 
+from nitime.timeseries import UniformTimeSeries 
+#Import the correlation analyis object
+from nitime.analysis import CorrelationAnalyzer
+#Import utility functions:
+from nitime.utils import percent_change
+from nitime.viz import matshow_roi
+
+#This information (the sampling rate) has to be known in advance:
+TR=1.89
+
+#Load the data from the csv file:
+data_rec = csv2rec('data/fmri_timeseries.csv')
+
+#Extract information:
+roi_names= np.array(data_rec.dtype.names)
+n_samples = data_rec.shape[0]
+
+
+#Make an empty container for the data
+data = np.zeros((len(roi_names),n_samples))
+
+for n_idx in range(len(roi_names)):
+   data[n_idx] = data_rec[roi_names[n_idx]]
+
+#Normalize the data:
+data = percent_change(data)
+
+#Initialize the time-series from the normalized data:
+T = UniformTimeSeries(data,sampling_interval=TR)
+T.metadata['roi'] = roi_names 
+
+#Initialize the correlation analyzer
+C = CorrelationAnalyzer(T)
+
+#Display the correlation matrix
+matshow_roi(C(),roi_names,size=[10.,10.])
+
+
---
+++ doc/examples/fmri2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+#Imports are as above:
+import numpy as np
+from matplotlib.pyplot import figure,legend
+from matplotlib.mlab import csv2rec
+from nitime.timeseries import UniformTimeSeries 
+from nitime.analysis import CorrelationAnalyzer
+from nitime.utils import percent_change
+import nitime.viz
+reload(nitime.viz)
+from nitime.viz import plot_xcorr
+
+#This part is the same as before
+TR=1.89
+data_rec = csv2rec('data/fmri_timeseries.csv')
+roi_names= np.array(data_rec.dtype.names)
+n_samples = data_rec.shape[0]
+data = np.zeros((len(roi_names),n_samples))
+
+for n_idx in range(len(roi_names)):
+   data[n_idx] = data_rec[roi_names[n_idx]]
+
+data = percent_change(data)
+T = UniformTimeSeries(data,sampling_interval=TR)
+T.metadata['roi'] = roi_names 
+C = CorrelationAnalyzer(T)
+
+#Extract the cross-correlation:
+xc = C.xcorr_norm
+
+idx_lcau = np.where(roi_names=='lcau')
+idx_rcau = np.where(roi_names=='rcau')
+idx_lput = np.where(roi_names=='lput')
+
+plot_xcorr(xc,((idx_lcau,idx_rcau),(idx_lcau,idx_lput)),
+               line_labels = ['rcau','lput'])
+
+
+
