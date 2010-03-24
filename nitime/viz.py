ab06daaa30563cd6d22a08483d9246d793b91f43
arokem <arokem@berkeley.edu>
2010-03-24 13:54:24 -0700

    Reapplied changes to viz

--- nitime/viz.py
+++ nitime/viz.py
@@ -1,12 +1,13 @@
 """Tools for visualization of time-series data.
 
-Depends on matplotlib.pyplot
+Depends on matplotlib
 
 
 """
 
 from nitime import timeseries as ts
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
 def plot_tseries(time_series,fig=None,axis=0,
@@ -44,23 +45,24 @@ def plot_tseries(time_series,fig=None,axis=0,
     else:
         ax = fig.get_axes()[axis]
 
+    #Make sure that time displays on the x axis with the units you want:
+    conv_fac = time_series.time._conversion_factor
+    this_time = time_series.time/float(conv_fac)
+    ax.plot(this_time,time_series.data.T)
+        
     if xlabel is None:
-        #Make sure that time displays on the x axis with the units you want:
-        conv_fac = time_series.time._conversion_factor
-        time_label = time_series.time/float(conv_fac)
-        ax.plot(time_label,time_series.data.T)
         ax.set_xlabel('Time (%s)' %time_series.time_unit) 
     else:
         ax.set_xlabel(xlabel)
 
+    
     if ylabel is not None:
         ax.set_ylabel(ylabel)
     
     return fig
 
 
-def matshow_roi(m,roi_names=None,fig=None,x_tick_rot=90,size=None,
-                cmap=plt.cm.PuBuGn):
+def matshow_roi(m,roi_names=None,fig=None,x_tick_rot=90,size=None):
     """This is the typical format to show a bivariate quantity (such as
     correlation or coherency between two different ROIs""" 
     N = len(roi_names)
@@ -78,8 +80,8 @@ def matshow_roi(m,roi_names=None,fig=None,x_tick_rot=90,size=None,
         fig.set_figheight(size[1])
 
     #The call to matshow produces the matrix plot:
-    plt.matshow(m,fignum=fig.number,cmap=cmap)
-    
+    plt.matshow(m,fignum=fig.number)
+
     #Formatting:
     ax = fig.axes[0]
     ax.set_xticks(np.arange(len(roi_names)))
@@ -95,11 +97,59 @@ def matshow_roi(m,roi_names=None,fig=None,x_tick_rot=90,size=None,
 
     for line in ax.yaxis.get_ticklines():
       line.set_markeredgewidth(0)
-
-    plt.colorbar()
-
+ 
     return fig
 
+def plot_xcorr(xc,ij,fig=None,line_labels=None,xticks=None,yticks=None,xlabel=None,ylabel=None):
+
+    """ Visualize the cross-correlation function"""
+   
+    if fig is None:
+        fig=plt.figure()
+
+    if not fig.get_axes():
+        ax = fig.add_subplot(1,1,1)
+    else:
+        ax = fig.get_axes()[axis]
+
+    if line_labels is not None:
+        #Reverse the order, so that pop() works:
+        line_labels.reverse()
+        this_labels = line_labels
 
 
+    #Make sure that time displays on the x axis with the units you want:
+    conv_fac = xc.time._conversion_factor
+    this_time = xc.time/float(conv_fac)
+    
+    for (i,j) in ij:
+        if this_labels is not None:
+            #Use pop() to get the first one and remove it:
+            ax.plot(this_time,xc[i,j].squeeze(),label=this_labels.pop())
+        else:
+            ax.plot(this_time,xc[i,j].squeeze())
+        
+    ax.set_xlabel('Time(sec)')
+    ax.set_ylabel('Correlation(normalized)')
+
+    if xlabel is None:
+        #Make sure that time displays on the x axis with the units you want:
+        conv_fac = xc.time._conversion_factor
+        time_label = xc.time/float(conv_fac)
+        ax.set_xlabel('Time (%s)' %xc.time_unit) 
+    else:
+        time_label = xlabel
+        ax.set_xlabel(xlabel)
+
+    if line_labels is not None:
+        plt.legend()
+
+    if ylabel is None:
+        ax.set_ylabel('Correlation')
+    else:
+        ax.set_ylabel(ylabel)
+    
+    return fig
+    
+    
 
