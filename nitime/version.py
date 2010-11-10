272a4e1e01996c8815e6f513c45d35d4787d3c3a
arokem <arokem@berkeley.edu>
2010-11-09 20:16:06 -0800

    Switched version number for release to 0.2

--- nitime/version.py
+++ nitime/version.py
@@ -5,9 +5,18 @@ ISRELEASED = False
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 2
-_version_micro = 'dev'
-__version__ = "%s.%s.%s" % (_version_major, _version_minor, _version_micro)
+_version_micro = '' # use '' for first of series, number for 1 and above
+_version_extra = 'dev'
+_version_extra = '' # Uncomment this for full releases
 
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
 
 CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Environment :: Console",
