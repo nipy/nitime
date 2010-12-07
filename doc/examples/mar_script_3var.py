e7ac11b4edae6454e6f10ef4ba6847b344da88ad
miketrumpis <mtrumpis@gmail.com>
2010-12-07 15:52:11 -0500

    3var script figure matches paper

--- doc/examples/mar_script_3var.py
+++ doc/examples/mar_script_3var.py
@@ -33,67 +33,9 @@ a2[0,1] = -0.2
 
 b = np.array([a1.copy(), a2.copy()])
 
-def transfer_function(a, Nfreqs=1024):
-
-    p = a.shape[0]
-    nc = a.shape[1]
-
-    ai = np.concatenate( (np.eye(nc).reshape(1,nc,nc), a), axis=0 )
-    ai.shape = (p+1, nc*nc)
-    # take the Fourier transform of each filter
-    af = []
-    for ai_t in ai.T:
-        w, ai_f = alg.my_freqz(ai_t, Nfreqs=Nfreqs)
-        af.append(ai_f)
-    Aw = np.array(af)
-    # now compose the fourier domain matrix for A(w)
-    nf = Aw.shape[-1]
-    Aw.shape = (nc, nc, nf)
-
-    Hw = np.empty_like(Aw)
-    for m in xrange(nf):
-        Hw[...,m] = np.linalg.inv(Aw[...,m])
-    return w, Hw
-
-def spectral_matrix(Hw, cov):
-    Sw = np.empty_like(Hw)
-    nf = Hw.shape[-1]
-    for m in xrange(nf):
-        H = Hw[...,m]
-        Sw[...,m] = np.dot(H, np.dot(cov, H.T.conj()))
-    return Sw
-
-def pairwise_causality(i, j, a, cov, Nfreqs=1024):
-    """Analyze the Granger causality between processes i and j within a
-    larger feedback system.
-
-    Parameters
-    ----------
-
-    i, j: int
-      The indices of the processes to analyze
-    a: ndarray, (P, nc, nc)
-      The order-P mAR coefficient matrix sequence
-    cov: ndarray, (nc, nc)
-      The covariance relationship between the innovations processes
-
-    Returns
-    -------
-
-    w: vector of frequencies in [0,PI]
-    f_i2j: Granger causality of i on j
-    f_j2i: Granger causality of j in i
-    """
-
-    a_ij_rows = a[:,[i,j]]
-    a_ij = a_ij_rows[:,:,[i,j]]
-    cov_ij_rows = cov[[i,j]]
-    cov_ij = cov_ij_rows[:,[i,j]]
-
-    w, f_i2j, f_j2i, _, _ = mar_xy_ana.granger_causality_xy(
-        a_ij, cov_ij, Nfreqs=Nfreqs
-        )
-    return w, f_i2j, f_j2i
+def extract_ij(i, j, m):
+    m_ij_rows = m[[i,j]]
+    return m_ij_rows[:,[i,j]]
 
 w, Haw = transfer_function(a)
 w, Hbw = transfer_function(b)
@@ -116,57 +58,81 @@ for i in xrange(N):
 
 Raxx = np.empty((N,3,3,3))
 Rbxx = np.empty((N,3,3,3))
+
 for i in xrange(N):
     Raxx[i] = mar_tools.autocov_vector(za[i], nlags=3)
     Rbxx[i] = mar_tools.autocov_vector(zb[i], nlags=3)
 
+# average trials together to find autocovariance estimate,
+# and extract pairwise components from the ac sequence
 Raxx = Raxx.mean(axis=0)
-Rbxx = Rbxx.mean(axis=0)
+xyRa = extract_ij(0, 1, Raxx)
+xzRa = extract_ij(0, 2, Raxx)
+yzRa = extract_ij(1, 2, Raxx)
 
+Rbxx = Rbxx.mean(axis=0)
+xyRb = extract_ij(0, 1, Rbxx)
+xzRb = extract_ij(0, 2, Rbxx)
+yzRb = extract_ij(1, 2, Rbxx)
+
+# now estimate mAR coefficients and covariance from the full and
+# pairwise relationships
 Raxx = Raxx.transpose(2,0,1)
 a_est, cov_est1 = mar_tools.lwr(Raxx)
+a_xy_est, cov_xy_est1 = mar_tools.lwr(xyRa.transpose(2,0,1))
+a_xz_est, cov_xz_est1 = mar_tools.lwr(xzRa.transpose(2,0,1))
+a_yz_est, cov_yz_est1 = mar_tools.lwr(yzRa.transpose(2,0,1))
 
 Rbxx = Rbxx.transpose(2,0,1)
 b_est, cov_est2 = mar_tools.lwr(Rbxx)
+b_xy_est, cov_xy_est2 = mar_tools.lwr(xyRb.transpose(2,0,1))
+b_xz_est, cov_xz_est2 = mar_tools.lwr(xzRb.transpose(2,0,1))
+b_yz_est, cov_yz_est2 = mar_tools.lwr(yzRb.transpose(2,0,1))
 
 fig = pp.figure()
 
-w, x2y_a, y2x_a = pairwise_causality(0, 1, a_est, cov_est1)
-w, x2y_b, y2x_b = pairwise_causality(0, 1, b_est, cov_est2)
+## w, x2y_a, y2x_a = pairwise_causality(0, 1, a_est, cov_est1)
+## w, x2y_b, y2x_b = pairwise_causality(0, 1, b_est, cov_est2)
+w, x2y_a, y2x_a, _, _ = mar_xy_ana.granger_causality_xy(a_xy_est, cov_xy_est1)
+w, x2y_b, y2x_b, _, _ = mar_xy_ana.granger_causality_xy(b_xy_est, cov_xy_est2)
 ax = fig.add_subplot(321)
-ax.plot(w, x2y_a, 'b')
-ax.plot(w, x2y_b, 'b--')
+ax.plot(w, x2y_a, 'b--')
+ax.plot(w, x2y_b, 'b')
 ax.set_title('x to y')
 ax.set_ylim((0,6))
 ax = fig.add_subplot(322)
-ax.plot(w, y2x_a, 'b')
-ax.plot(w, y2x_b, 'b--')
+ax.plot(w, y2x_a, 'b--')
+ax.plot(w, y2x_b, 'b')
 ax.set_title('y to x')
 ax.set_ylim((0,6))
 
-w, y2z_a, z2y_a = pairwise_causality(1, 2, a_est, cov_est1)
-w, y2z_b, z2y_b = pairwise_causality(1, 2, b_est, cov_est2)
+## w, y2z_a, z2y_a = pairwise_causality(1, 2, a_est, cov_est1)
+## w, y2z_b, z2y_b = pairwise_causality(1, 2, b_est, cov_est2)
+w, y2z_a, z2y_a, _, _ = mar_xy_ana.granger_causality_xy(a_yz_est, cov_yz_est1)
+w, y2z_b, z2y_b, _, _ = mar_xy_ana.granger_causality_xy(b_yz_est, cov_yz_est2)
 ax = fig.add_subplot(323)
-ax.plot(w, y2z_a, 'b')
-ax.plot(w, y2z_b, 'b--')
+ax.plot(w, y2z_a, 'b--')
+ax.plot(w, y2z_b, 'b')
 ax.set_title('y to z')
 ax.set_ylim((0,6))
 ax = fig.add_subplot(324)
-ax.plot(w, z2y_a, 'b')
-ax.plot(w, z2y_b, 'b--')
+ax.plot(w, z2y_a, 'b--')
+ax.plot(w, z2y_b, 'b')
 ax.set_title('z to y')
 ax.set_ylim((0,6))
 
-w, x2z_a, z2x_a = pairwise_causality(0, 2, a_est, cov_est1)
-w, x2z_b, z2x_b = pairwise_causality(0, 2, b_est, cov_est2)
+## w, x2z_a, z2x_a = pairwise_causality(0, 2, a_est, cov_est1)
+## w, x2z_b, z2x_b = pairwise_causality(0, 2, b_est, cov_est2)
+w, x2z_a, z2x_a, _, _ = mar_xy_ana.granger_causality_xy(a_xz_est, cov_xz_est1)
+w, x2z_b, z2x_b, _, _ = mar_xy_ana.granger_causality_xy(b_xz_est, cov_xz_est2)
 ax = fig.add_subplot(325)
-ax.plot(w, x2z_a, 'b')
-ax.plot(w, x2z_b, 'b--')
+ax.plot(w, x2z_a, 'b--')
+ax.plot(w, x2z_b, 'b')
 ax.set_title('x to z')
 ax.set_ylim((0,6))
 ax = fig.add_subplot(326)
-ax.plot(w, z2x_a, 'b')
-ax.plot(w, z2x_b, 'b--')
+ax.plot(w, z2x_a, 'b--')
+ax.plot(w, z2x_b, 'b')
 ax.set_title('z to x')
 ax.set_ylim((0,6))
 
