e06a0d8d9e273a93034f34e54739869dab8533da
arokem <arokem@berkeley.edu>
2011-04-27 16:52:55 -0700

    PEP8 compliance in nitime/algorithms/event_related.py

--- nitime/algorithms/event_related.py
+++ nitime/algorithms/event_related.py
@@ -7,34 +7,34 @@ Event-related analysis
 import numpy as np
 import scipy.linalg as linalg
 
-def fir(timeseries,design):
+
+def fir(timeseries, design):
     """
     Calculate the FIR (finite impulse response) HRF, according to [Burock2000]_
-    
-    
+
     Parameters
     ----------
-    
+
     timeseries : float array
             timeseries data
-    
+
     design : int array
           This is a design matrix.  It has to have shape = (number
           of TRS, number of conditions * length of HRF)
-    
-          The form of the matrix is: 
-            
-              A B C ... 
-          
+
+          The form of the matrix is:
+
+              A B C ...
+
           where A is a (number of TRs) x (length of HRF) matrix with a unity
           matrix placed with its top left corner placed in each TR in which
           event of type A occured in the design. B is the equivalent for
           events of type B, etc.
-    
-    Returns 
+
+    Returns
     -------
 
-    HRF: float array 
+    HRF: float array
         HRF is a numpy array of 1X(length of HRF * number of conditions)
         with the HRFs for the different conditions concatenated. This is an
         estimate of the linear filters between the time-series and the events
@@ -48,24 +48,24 @@ def fir(timeseries,design):
     .. math::
 
         \hat{h} = (X^T X)^{-1} X^T y
-        
-    .. [Burock2000] M.A. Burock and A.M.Dale (2000). Estimation and Detection of
-        Event-Related fMRI Signals with Temporally Correlated Noise: A
+
+    .. [Burock2000] M.A. Burock and A.M.Dale (2000). Estimation and Detection
+        of Event-Related fMRI Signals with Temporally Correlated Noise: A
         Statistically Efficient and Unbiased Approach. Human Brain Mapping,
         11:249-260
-         
+
     """
     X = np.matrix(design)
     y = np.matrix(timeseries)
-    h = np.array(linalg.pinv(X.T*X) * X.T*y.T)
-    return h   
+    h = np.array(linalg.pinv(X.T * X) * X.T * y.T)
+    return h
 
-def freq_domain_xcorr(tseries,events,t_before,t_after,Fs=1):
+
+def freq_domain_xcorr(tseries, events, t_before, t_after, Fs=1):
     """
     Calculates the  event related timeseries, using a cross-correlation in the
     frequency domain.
 
-    
     Parameters
     ----------
     tseries: float array
@@ -78,37 +78,35 @@ def freq_domain_xcorr(tseries,events,t_before,t_after,Fs=1):
        Time before the event to include
 
     t_after: float
-       Time after the event to include 
+       Time after the event to include
 
     Fs: float
        Sampling rate of the time-series (in Hz)
-    
+
     Returns
     -------
     xcorr: float array
         The correlation function between the tseries and the events. Can be
-        interperted as a linear filter from events to responses (the time-series)
-        of an LTI.  
-    
+        interperted as a linear filter from events to responses (the
+        time-series) of an LTI.
+
     """
-    
     fft = np.fft.fft
     ifft = np.fft.ifft
     fftshift = np.fft.fftshift
 
-    xcorr = np.real(fftshift ( ifft ( fft(tseries) * fft(np.fliplr([events]) )
-    ) ) )
-                     
-    return xcorr[0][ np.ceil(len(xcorr[0])/2)-t_before*Fs :
-                    np.ceil(len(xcorr[0])/2)+t_after/2*Fs ]/np.sum(events)
+    xcorr = np.real(fftshift(ifft(fft(tseries) *
+                                  fft(np.fliplr([events])))))
+
+    return xcorr[0][np.ceil(len(xcorr[0]) / 2) - t_before * Fs:
+                np.ceil(len(xcorr[0]) / 2) + t_after / 2 * Fs] / np.sum(events)
 
 
-def freq_domain_xcorr_zscored(tseries,events,t_before,t_after,Fs=1):
+def freq_domain_xcorr_zscored(tseries, events, t_before, t_after, Fs=1):
     """
     Calculates the z-scored event related timeseries, using a cross-correlation
     in the frequency domain.
 
-    
     Parameters
     ----------
     tseries: float array
@@ -121,32 +119,32 @@ def freq_domain_xcorr_zscored(tseries,events,t_before,t_after,Fs=1):
        Time before the event to include
 
     t_after: float
-       Time after the event to include 
+       Time after the event to include
 
     Fs: float
        Sampling rate of the time-series (in Hz)
-    
+
     Returns
     -------
     xcorr: float array
-    The correlation function between the tseries and the events. Can be
-    interperted as a linear filter from events to responses (the time-series)
-    of an LTI. Because it is normalized to its own mean and variance, it can be
-    interperted as measuring statistical significance relative to all
-    time-shifted versions of the events. 
-    
+        The correlation function between the tseries and the events. Can be
+        interperted as a linear filter from events to responses (the
+        time-series) of an LTI. Because it is normalized to its own mean and
+        variance, it can be interperted as measuring statistical significance
+        relative to all time-shifted versions of the events.
+
     """
-        
+
     fft = np.fft.fft
     ifft = np.fft.ifft
     fftshift = np.fft.fftshift
 
-    xcorr = np.real( fftshift ( ifft ( fft(tseries) * fft(np.fliplr([events]) )
-    ) ) )
+    xcorr = np.real(fftshift(ifft(fft(tseries) * fft(np.fliplr([events])))))
+
     meanSurr = np.mean(xcorr)
     stdSurr = np.std(xcorr)
-    
-    return ( ( (xcorr[0][ np.ceil(len(xcorr[0])/2)-t_before*Fs :
-                    np.ceil(len(xcorr[0])/2)+t_after*Fs ])
+
+    return (((xcorr[0][np.ceil(len(xcorr[0]) / 2) - t_before * Fs:
+                    np.ceil(len(xcorr[0]) / 2) + t_after * Fs])
              - meanSurr)
-             / stdSurr )
+             / stdSurr)
