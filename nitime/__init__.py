42584f47726221b43c76de0a23377c5c180fb4a9
Fernando Perez <Fernando.Perez@berkeley.edu>
2011-06-02 18:22:06 -0700

    Small documentation fixes

--- nitime/__init__.py
+++ nitime/__init__.py
@@ -1,19 +1,23 @@
 """
 Nitime: Time-series analysis for neuroscience
 
-The module has several sub-modules:
+Nitime has several sub-packages that are automatically available when ``import
+nitme`` is performed:
 
-- ``timeseries``: contains the constructors for time and time-series objects
+- ``timeseries``: contains the constructors for time and time-series objects.
 
 - ``algorithms``: Algorithms. This sub-module depends only on scipy,numpy and
   matplotlib. Contains various algorithms.
 
+- ``analysis``: Contains *Analyzer* objects, which implement particular
+  analysis methods on the time-series objects.
+
+The following subpackages are also available in nitime, but these must be
+individually imported with e.g. ``import nitime.viz``:
+
 - ``utils``: Utility functions.
 
-- ``analysis``: Contains *Analyzer* objects, which implement particular
-  analysis methods on the time-series objects
-
-- ``viz``: Vizualization
+- ``viz``: Vizualization.
 
 All of the sub-modules will be imported as part of ``__init__``, so that users
 have all of these things at their fingertips.
