d80cbaf23d42215fa01b772d34428d6bcc04faaf
arokem <arokem@berkeley.edu>
2011-06-15 15:02:17 -0700

    First pass at the GrangerAnalyzer, with internals.

--- nitime/analysis/granger.py
+++ nitime/analysis/granger.py
@@ -7,6 +7,7 @@ Analyzers for the calculation of Granger 'causality'
 import numpy as np
 import nitime.algorithms as alg
 import nitime.utils as utils
+from nitime import descriptors as desc
 
 from .base import BaseAnalyzer
 
@@ -65,81 +66,105 @@ def fit_model(x1, x2, order=None, max_order=10,
 
     return order, Rxx, coef, ecov
 
-## class GrangerAnalyzer(BaseAnalyzer):
-##     """Analyzer for computing all-to-all Granger 'causality' """
-##     def __init__(self, input=None, order=None, n_freqs=1024):
-##         """
-##         Initializer for the GrangerAnalyzer.
+class GrangerAnalyzer(BaseAnalyzer):
+    """Analyzer for computing all-to-all Granger 'causality' """
+    def __init__(self, input=None, order=None, n_freqs=1024, max_order=10,
+                 criterion=utils.bayesian_information_criterion):
+        """
+        Initializer for the GrangerAnalyzer.
 
-##         Parameters
-##         ----------
+        Parameters
+        ----------
 
-##         input: nitime TimeSeries object
+        input: nitime TimeSeries object
 
-##         order: int (optional)
-##              The order of the process. If this is not known, it will be
-##              estimated from the data, using the AIC
-##         n_freqs: int (optional)
-##             The size of the sampling grid in the frequency domain. Defaults to 1024
-
-
-##         """
-##         self._n_process = input.shape[0]
-##         self._n_freqs = n_freqs
-##         self._order = order
-
-##     @desc.setattr_on_read
-##     def order(self):
-##         if self._order is None:
-##             self.autocov_vector()
-##             return fit_order()
-##         else:
-##             return self._order
+        order: int (optional)
+             The order of the process. If this is not known, it will be
+             estimated from the data, using the AIC
+        n_freqs: int (optional)
+            The size of the sampling grid in the frequency domain. Defaults to 1024
 
 
-##     @desc.setattr_on_read
-##     def autocov_vector(self):
-##         """
+        """
+        self.data = input.data
+        self.sampling_rate = input.sampling_rate
+        self._n_process = input.shape[0]
+        self._n_freqs = n_freqs
+        self._order = order
+        self._criterion = criterion
+        self._max_order = max_order
 
+    @desc.setattr_on_read
+    def model(self):
+        model = dict(order={}, autocov={}, model_coef={}, error_cov={})
+        for i in xrange(self._n_process):
+            for j in xrange(i+1, self._n_process):
+                model[i, j] = {}
+                order_t, Rxx_t, coef_t, ecov_t = fit_model(self.data[i],
+                                                           self.data[j],
+                                                           order=self._order,
+                                                           max_order=self._max_order,
+                                                           criterion=self._criterion)
+                model['order'][i, j] = order_t
+                model['autocov'][i, j] = Rxx_t
+                model['model_coef'][i, j] = coef_t
+                model['error_cov'][i, j] = ecov_t
 
-##         """
-##         Rxx = np.zeros((self._n_process,
-##                         self._n_process,
-##                         self.order), dtype=complex)
+        return model
 
-##         for i in xarange(self._n_process):
-##             for j in xrange(i, self._n_process):
-##                 Rxx[i, j] = autocov_vector(x, nlags=self.order)
+    @desc.setattr_on_read
+    def order(self):
+        if self._order is None:
+            return self.model['order']
+        else:
+            order = {}
+            for i in xrange(self._n_process):
+                for j in xrange(i+1, self._n_process):
+                    order[i, j] = self._order
+            return order
 
-##     @desc.setattr_on_read
-##     def coefs(self):
-##         coefs = np.zeros((self._n_process,
-##                           self._n_process,
-##                           self.order), dtype=complex)
+    @desc.setattr_on_read
+    def autocov(self):
+        return self.model['autocov']
 
-##         ecov = np.zeros((self._n_process,
-##                          self._n_process,
-##                          self.order), dtype=complex)
+    @desc.setattr_on_read
+    def model_coef(self):
+        return self.model['model_coef']
 
-##         for i in xrange(self._n_process):
-##             for j in xrange(i, self._n_process):
-##                 # Estimate the autorgressive process using LWR recursion:
-##                 coefs[i, j], ecov[i, j] = alg.lwr_recursion(Rxx)
-##     def granger_caus
-##     w, f_x2y, f_y2x, f_xy, Sw = alg.granger_causality_xy(self.coefs,
-##                                                          self.ecov,
-##                                                          n_freqs=n_freqs)
+    @desc.setattr_on_read
+    def error_cov(self):
+        return self.model['error_cov']
 
-##     @desc.setattr_on_read
-##     def causality_xy(self):
+    @desc.setattr_on_read
+    def granger_causality(self):
+        gc = dict(frequencies={}, gc_xy={}, gc_yx={}, gc_sim={}, spectral_density={})
+        for i in xrange(self._n_process):
+            for j in xrange(i+1, self._n_process):
+                w, f_x2y, f_y2x, f_xy, Sw = \
+                alg.granger_causality_xy(self.model_coef[i, j ],
+                                         self.error_cov[i, j],
+                                         n_freqs=self._n_freqs)
 
-##     @desc.setattr_on_read
-##     def causality_yx(self):
+                gc['frequencies'][i, j] = w
+                gc['gc_xy'][i, j] = f_x2y
+                gc['gc_yx'][i, j] = f_y2x
+                gc['gc_sim'][i, j] = f_xy
+                gc['spectral_density'][i, j] = Sw
 
-##     @desc.setattr_on_read
-##     def simultaneous_causality(self):
+        return gc
 
-##     @desc.setattr_on_read
-##     def order(self):
-##         if self.order is None:
-##             # Calculate the order with BIC:
+    @desc.setattr_on_read
+    def frequencies(self):
+        return self.granger_causality['frequencies']
+
+    @desc.setattr_on_read
+    def causality_xy(self):
+        return self.granger_causality['gc_xy']
+
+    @desc.setattr_on_read
+    def causality_yx(self):
+        return self.granger_causality['gc_yx']
+
+    @desc.setattr_on_read
+    def simultaneous_causality(self):
+        return self.granger_causality['gc_sim']
