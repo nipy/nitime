ENH: change up adaptive weighting method, probably fixes bugs, and should improve convergence for a high number of points

nitime/utils.py
@@ -514,7 +514,8 @@ def adaptive_weights(yk, eigvals, sides='onesided', max_iter=150):
     N = yk.shape[1]
     sdf = mtm_cross_spectrum(yk, yk, eigvals[:, None], sides=sides)
     L = sdf.shape[-1]
-    var_est = np.trapz(sdf, dx=np.pi / L) / (2 * np.pi)
+    var_est = np.sum(sdf, axis=-1) / N
+    bband_sup = (1-eigvals)*var_est
 
     # The process is to iteratively switch solving for the following
     # two expressions:
@@ -533,30 +534,46 @@ def adaptive_weights(yk, eigvals, sides='onesided', max_iter=150):
     sdf_iter = mtm_cross_spectrum(yk[:2], yk[:2], eigvals[:2, None],
                                   sides=sides)
     err = np.zeros((K, L))
+    # for numerical considerations, don't bother doing adaptive
+    # weighting after 150 dB down
+    min_pwr = sdf_iter.max() * 10 ** (-150/20.)
+    default_weights = np.where(sdf_iter < min_pwr)[0]
+    adaptiv_weights = np.where(sdf_iter >= min_pwr)[0]
+
+    w_def = rt_eig[:,None] * sdf_iter[default_weights]
+    w_def /= eigvals[:, None] * sdf_iter[default_weights] + bband_sup[:,None]
+
+    d_sdfs = np.abs(yk[:,adaptiv_weights])**2
+    if L < N:
+        d_sdfs *= 2
+    sdf_iter = sdf_iter[adaptiv_weights]
+    yk = yk[:,adaptiv_weights]
     for n in range(max_iter):
-        d_k = sdf_iter[None, :] / (eigvals[:, None] * sdf_iter[None, :] + \
-                                  (1 - eigvals[:, None]) * var_est)
-        d_k *= rt_eig[:, None]
+        d_k = rt_eig[:,None] * sdf_iter[None, :]
+        d_k /= eigvals[:, None]*sdf_iter[None, :] + bband_sup[:,None]
         # Test for convergence -- this is overly conservative, since
         # iteration only stops when all frequencies have converged.
         # A better approach is to iterate separately for each freq, but
         # that is a nonvectorized algorithm.
-        # Take the RMS difference in weights from the previous iterate
-        # across frequencies. If the maximum RMS error across freqs is
-        # less than 1e-10, then we're converged
-        err -= d_k
-        if (err ** 2).mean(axis=0).max() < 1e-10:
+        #sdf_iter = mtm_cross_spectrum(yk, yk, d_k, sides=sides)
+        sdf_iter = np.sum( d_k**2 * d_sdfs, axis=0 )
+        sdf_iter /= np.sum( d_k**2, axis=0 )
+        # Compute the cost function from eq 5.4 in Thomson 1982
+        cfn = eigvals[:,None] * (sdf_iter[None,:] - d_sdfs)
+        cfn /= (eigvals[:,None] * sdf_iter[None,:] + bband_sup[:,None])**2
+        cfn = np.sum(cfn, axis=0)
+        # there seem to be some pathological freqs sometimes ..
+        # this should be a good heuristic
+        if np.percentile(cfn**2, 95) < 1e-12:
             break
-        # update the iterative estimate with this d_k
-        sdf_iter = mtm_cross_spectrum(yk, yk, d_k, sides=sides)
-        err = d_k
     else:  # If you have reached maximum number of iterations
         # Issue a warning and return non-converged weights:
         e_s = 'Breaking due to iterative meltdown in '
         e_s += 'nitime.utils.adaptive_weights.'
         warnings.warn(e_s, RuntimeWarning)
-
-    weights = d_k
+    weights = np.zeros( (K,L) )
+    weights[:,adaptiv_weights] = d_k
+    weights[:,default_weights] = w_def
     nu = 2 * (weights ** 2).sum(axis=-2)
     return weights, nu
 