8147a6c8c2eef34482638ea859face0a58f5505c
miketrumpis <mtrumpis@gmail.com>
2013-10-14 14:32:27 -0400

    Do conditional build of the Cython module. This build works at least down to Cython 0.14

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ if os.path.exists('MANIFEST'):
     os.remove('MANIFEST')
 
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext as build_pyx_ext
-from numpy import get_include
 
 # Get version and release info, which is all stored in nitime/version.py
 ver_file = os.path.join('nitime', 'version.py')
@@ -36,11 +33,18 @@ opts = dict(name=NAME,
             requires=REQUIRES,
             )
 
-# add Cython extensions to the setup options
-exts = [ Extension('nitime._utils', ['nitime/_utils.pyx'],
-                   include_dirs=[get_include()]) ]
-opts['cmdclass'] = dict(build_ext=build_pyx_ext)
-opts['ext_modules'] = exts
+try:
+    from distutils.extension import Extension
+    from Cython.Distutils import build_ext as build_pyx_ext
+    from numpy import get_include
+    # add Cython extensions to the setup options
+    exts = [ Extension('nitime._utils', ['nitime/_utils.pyx'],
+                       include_dirs=[get_include()]) ]
+    opts['cmdclass'] = dict(build_ext=build_pyx_ext)
+    opts['ext_modules'] = exts
+except ImportError:
+    # no loop for you!
+    pass
 
 # For some commands, use setuptools.  Note that we do NOT list install here!
 # If you want a setuptools-enhanced install, just run 'setupegg.py install'
