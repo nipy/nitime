BF: Fixed spectral normalization. Now we adopt a consistent Watts/Hz standard for any combination of sampling frequency and FFT-length

nitime/algorithms/spectral.py
@@ -186,14 +186,15 @@ def get_spectra_bi(x, y, method=None):
     return f, fxx, fyy, fxy
 
 
-# The following spectrum estimates are normalized to the following convention..
-# By definition, Sxx(w) = DTFT{Rxx(n)}, where Rxx(n) is the autocovariance
+# The following spectrum estimates are normalized to the convention
+# adopted by MATLAB (or at least spectrum.psd)
+# By definition, Sxx(f) = DTFT{Rxx(n)}, where Rxx(n) is the autocovariance
 # function of x(n). Therefore the integral from
-# [-PI, PI] of Sxx(w)*dw/(2PI) is Rxx(0).
+# [-Fs/2, Fs/2] of Sxx(f)*df is Rxx(0).
 # And from the definition of Rxx(n),
 # Rxx(0) = Expected-Value{x(n)x*(n)} = Expected-Value{ |x|^2 },
 # which is estimated as (x*x.conj()).mean()
-# In other words, sum(Sxx)/NFFT = var(x)
+# In other words, sum(Sxx) * Fs / NFFT ~ var(x)
 
 def periodogram(s, Fs=2 * np.pi, Sk=None, N=None,
                 sides='default', normalize=True):
@@ -231,11 +232,6 @@ def periodogram(s, Fs=2 * np.pi, Sk=None, N=None,
        f: The central frequencies for the frequency bands
        PSD estimate for each row of s
 
-    Notes
-    -----
-    setting dw = 2*PI/N, then the integral from -PI, PI (or 0,PI) of PSD/(2PI)
-    will be nearly equal to sxx(0), where sxx is the autocovariance function
-    of s(n). By definition, sxx(0) = E{s(n)s*(n)} ~ (s*s.conj()).mean()
     """
     if Sk is not None:
         N = Sk.shape[-1]
@@ -243,7 +239,6 @@ def periodogram(s, Fs=2 * np.pi, Sk=None, N=None,
         N = s.shape[-1] if not N else N
         Sk = fftpack.fft(s, n=N)
     pshape = list(Sk.shape)
-    norm = float(s.shape[-1])
 
     # if the time series is a complex vector, a one sided PSD is invalid:
     if (sides == 'default' and np.iscomplexobj(s)) or sides == 'twosided':
@@ -267,7 +262,7 @@ def periodogram(s, Fs=2 * np.pi, Sk=None, N=None,
         P = (Sk * Sk.conj()).real
         freqs = np.linspace(0, Fs, N, endpoint=False)
     if normalize:
-        P /= norm
+        P /= (Fs * s.shape[-1])
     return freqs, P
 
 
@@ -314,12 +309,6 @@ def periodogram_csd(s, Fs=2 * np.pi, Sk=None, NFFT=None, sides='default',
         holding Sij(f). For an input array that is reshaped to (M,N),
         the output is (M,M,N)
 
-    Notes
-    -----
-    setting dw = 2*PI/N, then the integral from -PI, PI (or 0,PI) of PSD/(2PI)
-    will be nearly equal to sxy(0), where sxx is the crosscovariance function
-    of s1(n), s2(n). By definition, sxy(0) = E{s1(n)s2*(n)} ~
-    (s1*s2.conj()).mean()
     """
     s_shape = s.shape
     s.shape = (np.prod(s_shape[:-1]), s_shape[-1])
@@ -339,7 +328,6 @@ def periodogram_csd(s, Fs=2 * np.pi, Sk=None, NFFT=None, sides='default',
     s.shape = s_shape
 
     M = Sk_loc.shape[0]
-    norm = float(s.shape[-1])
 
     # if the time series is a complex vector, a one sided PSD is invalid:
     if (sides == 'default' and np.iscomplexobj(s)) or sides == 'twosided':
@@ -370,7 +358,7 @@ def periodogram_csd(s, Fs=2 * np.pi, Sk=None, NFFT=None, sides='default',
             for j in xrange(i + 1):
                 csd_pairs[i, j] = Sk_loc[i] * Sk_loc[j].conj()
     if normalize:
-        csd_pairs /= norm
+        csd_pairs /= (Fs*N)
 
     csd_mat = csd_pairs.transpose(1,0,2).conj()
     csd_mat += csd_pairs
@@ -796,7 +784,8 @@ def multi_taper_psd(
     sdf_est = mtm_cross_spectrum(
         spectra, spectra, weights, sides=sides
         )
-
+    sdf_est /= Fs
+    
     if sides == 'onesided':
         freqs = np.linspace(0, Fs / 2, NFFT / 2 + 1)
     else:
@@ -934,7 +923,8 @@ def multi_taper_csd(s, Fs=2 * np.pi, NW=None, BW=None, low_bias=True,
     csdfs += csd_pairs
     diag_idc = (np.arange(M), np.arange(M))
     csdfs[diag_idc] /= 2
-
+    csdfs /= Fs
+    
     if sides == 'onesided':
         freqs = np.linspace(0, Fs / 2, NFFT / 2 + 1)
     else:

nitime/algorithms/tests/test_spectral.py
@@ -386,11 +386,16 @@ def test_hermitian_periodogram_csd():
         for j in xrange(i+1):
             xc1 = csd1[i,j]
             xc2 = csd1[j,i]
-            npt.assert_equal(xc1, xc2.conj())
+            npt.assert_equal(
+                xc1, xc2.conj(), err_msg='Periodogram CSD not Hermitian'
+                )
 
     _, psd = tsa.periodogram(sig)
     for i in xrange(4):
-        npt.assert_equal(psd[i], csd1[i,i].real)
+        npt.assert_almost_equal(
+            psd[i], csd1[i,i].real,
+            err_msg='Periodgram CSD diagonal inconsistent with real PSD'
+            )
 
 def test_hermitian_multitaper_csd():
     """
@@ -400,14 +405,75 @@ def test_hermitian_multitaper_csd():
 
     sig = np.random.randn(4,256)
 
-    _, csd1 = tsa.multi_taper_csd(sig)
+    _, csd1 = tsa.multi_taper_csd(sig, adaptive=False)
 
     for i in xrange(4):
         for j in xrange(i+1):
             xc1 = csd1[i,j]
             xc2 = csd1[j,i]
-            npt.assert_equal(xc1, xc2.conj())
+            npt.assert_equal(
+                xc1, xc2.conj(), err_msg='MTM CSD not Hermitian'
+                )
 
-    _, psd, _ = tsa.multi_taper_psd(sig)
+    _, psd, _ = tsa.multi_taper_psd(sig, adaptive=False)
     for i in xrange(4):
-        npt.assert_equal(psd[i], csd1[i,i].real)
+        npt.assert_almost_equal(
+            psd[i], csd1[i,i].real,
+            err_msg='MTM CSD diagonal inconsistent with real PSD'
+            )
+
+def test_periodogram_spectral_normalization():
+    """
+    Check that the spectral estimators are normalized in the
+    correct Watts/Hz fashion
+    """
+
+    x = np.random.randn(1024)
+    f1, Xp1 = tsa.periodogram(x)
+    f2, Xp2 = tsa.periodogram(x, Fs=100)
+    f3, Xp3 = tsa.periodogram(x, N=2**12)
+
+    p1 = np.sum(Xp1) * 2 * np.pi / 2**10
+    p2 = np.sum(Xp2) * 100 / 2**10
+    p3 = np.sum(Xp3) * 2 * np.pi / 2**12
+    nt.assert_true( np.abs(p1 - p2) < 1e-14,
+                    'Inconsistent frequency normalization in periodogram (1)' )
+    nt.assert_true( np.abs(p3 - p2) < 1e-8,
+                    'Inconsistent frequency normalization in periodogram (2)' )
+
+    td_var = np.var(x)
+    # assure that the estimators are at least in the same
+    # order of magnitude as the time-domain variance
+    nt.assert_true( np.log10(p1/td_var) < 1,
+                    'Incorrect frequency normalization in periodogram' )
+
+    # check the freq vector while we're here
+    nt.assert_true( f2.max() == 50, 'Periodogram returns wrong frequency bins' )
+
+def test_multitaper_spectral_normalization():
+    """
+    Check that the spectral estimators are normalized in the
+    correct Watts/Hz fashion
+    """
+
+    x = np.random.randn(1024)
+    f1, Xp1, _ = tsa.multi_taper_psd(x)
+    f2, Xp2, _ = tsa.multi_taper_psd(x, Fs=100)
+    f3, Xp3, _ = tsa.multi_taper_psd(x, NFFT=2**12)
+
+    p1 = np.sum(Xp1) * 2 * np.pi / 2**10
+    p2 = np.sum(Xp2) * 100 / 2**10
+    p3 = np.sum(Xp3) * 2 * np.pi / 2**12
+    nt.assert_true( np.abs(p1 - p2) < 1e-14,
+                    'Inconsistent frequency normalization in MTM PSD (1)' )
+    nt.assert_true( np.abs(p3 - p2) < 1e-8,
+                    'Inconsistent frequency normalization in MTM PSD (2)' )
+
+    td_var = np.var(x)
+    # assure that the estimators are at least in the same
+    # order of magnitude as the time-domain variance
+    nt.assert_true( np.log10(p1/td_var) < 1,
+                    'Incorrect frequency normalization in MTM PSD' )
+
+    # check the freq vector while we're here
+    nt.assert_true( f2.max() == 50, 'MTM PSD returns wrong frequency bins' )