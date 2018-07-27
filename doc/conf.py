eea6013485896b56ae975461dda0a247ab2908cf
Chris Markiewicz <markiewicz@stanford.edu>
2018-07-27 17:43:19 -0400

    DOC: Update copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -86,7 +86,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nitime'
-copyright = u'2009, Neuroimaging in Python team'
+copyright = u'2018, Neuroimaging in Python team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
