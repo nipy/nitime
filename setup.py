3a6cf4ab60a6403f6b5fa977d4d2df8918fab058
Chris Markiewicz <effigies@gmail.com>
2023-06-14 11:18:26 -0400

    MNT: Move most build metadata into pyproject.toml

--- setup.py
+++ setup.py
@@ -2,50 +2,14 @@
 """Setup file for the Python nitime package."""
 
 import os
-import sys
+from runpy import run_path
 
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 # Get version and release info, which is all stored in nitime/version.py
-ver_file = os.path.join('nitime', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+VARS = run_path(os.path.join('nitime', 'version.py'))
 
-REQUIRES = []
-
-with open('requirements.txt') as f:
-    ll = f.readline()[:-1]
-    while ll:
-        REQUIRES.append(ll)
-        ll = f.readline()[:-1]
-
-PACKAGES = find_packages()
-
-
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            install_requires=REQUIRES,
-            requires=REQUIRES,
-            python_requires=PYTHON_REQUIRES,
-            )
+opts = {'version': VARS['__version__']}
 
 try:
     from setuptools import Extension
