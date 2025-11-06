5b3635aa63fddb6c22d8bde45f8c6cdf88d64145
Eric Larson <larson.eric.d@gmail.com>
2025-11-06 09:12:04 -0500

    MAINT: Build abi3 wheels and use native arm64 Linux

--- setup.py
+++ setup.py
@@ -4,26 +4,46 @@
 This file only contains cython components.
 See pyproject.toml for the remaining configuration.
 """
+import platform
+import sys
+
 from setuptools import setup
+from setuptools import Extension
+from Cython.Build import cythonize
+from numpy import get_include
 
-try:
-    from setuptools import Extension
-    from Cython.Build import cythonize
-    from numpy import get_include
+# add Cython extensions to the setup options
 
-    # add Cython extensions to the setup options
-    exts = [
-        Extension(
-            'nitime._utils',
-            ['nitime/_utils.pyx'],
-            include_dirs=[get_include()],
-            define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-        )
-    ]
-    opts = {'ext_modules': cythonize(exts, language_level='3')}
-except ImportError:
-    # no loop for you!
-    opts = {}
+
+# https://github.com/joerick/python-abi3-package-sample/blob/main/setup.py
+class bdist_wheel_abi3(bdist_wheel):  # noqa: D101
+    def get_tag(self):  # noqa: D102
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp311", "abi3", plat
+
+        return python, abi, plat
+
+macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+ext_kwargs = {}
+setup_kwargs = {}
+if sys.version_info.minor >= 11 and platform.python_implementation() == "CPython":
+    # Can create an abi3 wheel (typed memoryviews first available in 3.11)!
+    macros.append(("Py_LIMITED_API", "0x030B0000"))
+    ext_kwargs["py_limited_api"] = True
+    setup_kwargs["cmdclass"] = {"bdist_wheel": bdist_wheel_abi3}
+
+
+exts = [
+    Extension(
+        'nitime._utils',
+        ['nitime/_utils.pyx'],
+        include_dirs=[get_include()],
+        define_macros=macros,
+    )
+]
+opts = {'ext_modules': cythonize(exts, language_level='3'), **setup_kwargs}
 
 # Now call the actual setup function
 if __name__ == '__main__':
